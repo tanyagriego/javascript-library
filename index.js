95c970c841987619e2d7cce6ac722dd77196185c
tanyagriego <tanya.griego@gmail.com>
2018-10-07 12:46:38 -0700

    Created functions to fetch data from YouTube and Songsterr APIS

--- index.js
+++ index.js
@@ -22,10 +22,6 @@ function getSongsterrData (searchTerm, callback){
     $.ajax(settings);
 }
 
-
-
-
-
 //This function gets data from the YouTube API
 function getYouTubeData (searchTerm, callback){
     const settings = {
@@ -44,34 +40,3 @@ function getYouTubeData (searchTerm, callback){
     $.ajax(settings);
 }
 
-//This function appends the API data to the DOM
-function appendYouTubeApiData (item){
-    return`<div class="search-results"><a href="https://www.youtube.com/watch?v=${item.id.videoId} target="_blank"><img src="${item.snippet.thumbnails.default.url}"></a>
-           <a href="https://www.youtube.com/watch?v=${item.id.videoId} class="videoTitle" target="_blank">${item.snippet.title}</a></div>`;
-}
-
-
-//This function does something with the data (callack)
-function displayApiData (data) {
-    console.log(data.items[0]);
-    //loop through array items in object data?
-    const results = data.items.map((item) => { 
-    //render results to the function that creates html
-        return appendYouTubeApiData (item);
-    //put the html into the search results div
-    });
-    $('.search_results').html(results);
-}
-
-//This function allows the user to submit their search results
-function submit () {
-    $('.js-search-form').submit(event => {
-        event.preventDefault();
-        const searchTermTextbox = $(event.currentTarget).find('.js-query');
-        const query = searchTermTextbox.val();
-        searchTermTextbox.val("");
-        getYouTubeData(query, displayApiData);
-    });
-}
-
-$(submit);
