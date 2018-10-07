35ff63d28887c907853770679f9cf5bef9a183ab
tanyagriego <tanya.griego@gmail.com>
2018-10-07 14:24:27 -0700

    Added functionality to display YouTube results.

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const Songsterr_Search_URL = "http://www.songsterr.com/a/rest/"
 //This function gets data from the songsterr API
 function getSongsterrData (searchTerm, callback){
     const settings = {
-        url: YouTube_Search_URL,
+        url: Songsterr_Search_URL,
         data: {
             q: `${searchTerm}`,
             part: '',
             key: "",
             per_page: 3
-
         },
+
         dataType: 'json',
         type: "GET",
         success: callback
@@ -27,7 +27,7 @@ function getYouTubeData (searchTerm, callback){
     const settings = {
         url: YouTube_Search_URL,
         data: {
-            q: `${searchTerm}`,
+            q: `guitarlesson${searchTerm}`,
             part: 'snippet',
             key: "AIzaSyBXjnIeLhAmsGhwe7XQePKmHvCL1J_DEMM",
             per_page: 3
@@ -40,3 +40,33 @@ function getYouTubeData (searchTerm, callback){
     $.ajax(settings);
 }
 
+//This function appends the API data to the DOM
+function appendYouTubeApiData (item){
+    return`<div class="search-results"><a href="https://www.youtube.com/watch?v=${item.id.videoId} target="_blank"><img src="${item.snippet.thumbnails.default.url}"></a>
+           <a href="https://www.youtube.com/watch?v=${item.id.videoId} class="videoTitle" target="_blank">${item.snippet.title}</a></div>`;
+}
+
+//This function does something with the data (callack)
+function displayApiData (data) {
+    console.log(data.items[0]);
+    //loop through array items in object data?
+    const results = data.items.map((item) => { 
+    //render results to the function that creates html
+        return appendYouTubeApiData (item);
+    //put the html into the search results div
+    });
+    $('.search_results').html(results);
+}
+
+//This function allows the user to submit their search results
+function submit () {
+    $('.js-search-form').submit(event => {
+        event.preventDefault();
+        const searchTermTextbox = $(event.currentTarget).find('.js-query');
+        const query = searchTermTextbox.val();
+        searchTermTextbox.val("");
+        getYouTubeData(query, displayApiData);
+    });
+}
+
+$(submit);
